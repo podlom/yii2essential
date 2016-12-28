70d5bb321568f1a967c5ccdf62df1f2599ff3b73
Taras Shkodenko <taras@shkodenko.com>
2016-12-28 17:41:10 +0200

    Home work #2 Lesson #5

---
+++ advanced/frontend/controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace frontend\controllers;
+
+class PageController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionView()
+    {
+        return $this->render('view');
+    }
+
+}
