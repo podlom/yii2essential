ce78d518e4c32d078f787c725ad79c12fc7b1a54
Taras Shkodenko <taras@shkodenko.com>
2016-12-28 17:27:43 +0200

    Home work #1 Lesson #5

---
+++ advanced/common/models/Page.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "page".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ * @property string $added_at
+ * @property string $updated_at
+ */
+class Page extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'page';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content', 'updated_at'], 'required'],
+            [['content'], 'string'],
+            [['added_at', 'updated_at'], 'safe'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'added_at' => 'Added At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
