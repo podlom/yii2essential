bec7526f3901cce1bf50f1b40dada45bf654ada1
Taras Shkodenko <taras@shkodenko.com>
2016-12-28 16:26:55 +0200

    Home work #2 Lesson #4

---
+++ advanced/console/migrations/m161228_141231_page.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m161228_141231_page extends Migration
+{
+    public function up()
+    {
+        $this->execute("DROP TABLE IF EXISTS `page`");
+
+        $this->createTable('page', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'content' => Schema::TYPE_TEXT . ' NOT NULL',
+            'added_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
+            'updated_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL',
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('page');
+    }
+}
